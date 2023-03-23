0788ecedcbeb276a818b8694e5ab2ece79aa6d4d
Devang0907 <116096508+Devang0907@users.noreply.github.com>
2023-03-23 20:46:47 +0530

    Update hosteler.java

--- hosteler.java
+++ hosteler.java
@@ -1,12 +1,27 @@
+import java.util*;
 public class hosteler {
     public static void main(String args[]){
+        Hostel h=new Hostel();
+                
 }
     
 }
 
 class Hostel{
+    String name;
+    String surname;
+    int age;
+    String collage;
+    String city;
+    String mobile_no;
+    String email_id;
+    char room;
+    int flag=0;
+    Scanner sc=new Scanner(System.in);
+    
 
 Hostel(){
+        While(flag==0){
         System.out.println("Welcome to Hostel portal");
         
         System.out.println("press 1:to get information about us");
@@ -17,26 +32,33 @@ Hostel(){
 
         int choice=s.nextInt();
    
-        if(choice==1){infoUs();} 
-
+        if(choice==1){infoUs();}
+        if(choice==2){admission();}
+        if(choice==3){fees();}
+        if(choice==4){feedback();}
+        if(choice==5){flag=1;}
+        else{   
+            System.out.println("Enter valid choice.");
+            }  
+        }
 }
         void infoUs(){
         System.out.println("press 1:to get information about fee Structure");
         System.out.println("press 2:to get information about Mess");
         System.out.println("press 3:to get information about Services");
         System.out.println("press 4:to get information about Rules & Regulations");
-        System.out.println("press 5:to go Back");
+        System.out.println("press 5:to go Back"); 
 
         int choice_1=s.nextInt();
         if(choice_1==1){
             System.out.println("Fee Structure :");
-            System.out.println("No. of Sharing     Fees/month    A.C./Non A.C.");
-            System.out.println("_______________________________________________");
-            System.out.println("    2               12,000             A.C.   ");
-            System.out.println("    2               10,000           Non A.C. ");
-            System.out.println("    4               9,000              A.C.   ");
-            System.out.println("    4               8,000            Non A.C  ");
-            System.out.println("    6               6,000            Non A.C  ");
+            System.out.println("  Room type     No. of Sharing     Fees/month    A.C./Non A.C.");
+            System.out.println("______________________________________________________________");
+            System.out.println("    A        2               12,000                  A.C.   ");
+            System.out.println("    B        2               10,000                Non A.C. ");
+            System.out.println("    C        4               9,000                   A.C.   ");
+            System.out.println("    D        4               8,000                 Non A.C  ");
+            System.out.println("    G        6               6,000                 Non A.C  ");
 
         }
         if(choice_1==2){
@@ -65,11 +87,60 @@ Hostel(){
             
                                 
                        }
-        if(choice_1==5){}
+            else{
+               System.out.println("Enter valid choice.");
+           }
+        }
+    
+}
+
+        void admission(){
+            System.out.println("Enter your name :");
+            name=sc.next();
+            System.out.println("Enter your surname :");
+            surname=sc.next();
+            System.out.println("Enter your age :");
+            age=sc.nextInt();
+            System.out.println("Enter your collage :");
+            collage=sc.next();
+            System.out.println("Enter your city :");
+            city=sc.next();
+            System.out.println("Enter your phone no. :");
+            mobile_no=sc.next();
+            System.out.println("Enter your email id :");
+            email_id=sc.next();
+            System.out.println("Enter room type :");
+            room=sc.nextLine();
+               
             
 
-
 }
+
+
+       void fees(){
+           System.out.println("Enter 1:for online payment ");
+           System.out.println("Enter 2:for offline payment(cash only) ");
+           int c=sc.nextInt();
+           
+           if(c==1){
+               System.out.println("You can pay using upi by given upi id");
+               System.out.println("hosteler@123");
+           }
+           if(c==2){
+               System.out.println("You can pay fees to our office.");
+           }
+           else{
+               System.out.println("Enter valid choice.");
+           }
+       }
+           
+           void feedback(){
+               
+               System.out.println("Enter your feedback below :");
+               String feedback=sc.next();
+               
+               
+    
 
        
 
